ea615a39d75a1a0d53acbc8b7e3aa8d9c9e77b98
gamutsf <ian@Ians-MacBook-Pro-2.local>
2014-09-10 11:22:59 -0700

    + Added fonts

--- views/single/single.js
+++ views/single/single.js
@@ -11,5 +11,20 @@ SingleView.prototype.onDOMReady = function ()
 	console.log ("SingleView.onDOMReady()");
 	
 	//window.scrollTo(0, 0);
+	
+	/**************/
+	// Scrolling
+	/**************/
+  
+  // Fix scrolling in details section
+  var scrollable = document.querySelector(".single");
+	new ScrollFix(scrollable);
+	
+	// Prevent event page from moving while open
+	/*
+	document.querySelector('.location-more').addEventListener('touchmove', function (e) { 
+		e.preventDefault(); 
+	});
+	*/
 }
 
